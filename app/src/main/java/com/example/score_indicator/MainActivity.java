e3f9a34b01328cab5e0a6008a5ec833af6ed9b85
Cheshiray <cheshiray@163.com>
2020-09-19 06:37:44 +0800

    score-indicator

--- app/src/main/java/com/example/score_indicator/MainActivity.java
+++ app/src/main/java/com/example/score_indicator/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.score_indicator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        tv = findViewById(R.id.textView);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer a = Integer.valueOf(tv.getText().toString())+3;
+                tv.setText(a.toString());
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer b = Integer.valueOf(tv.getText().toString())+2;
+                tv.setText(b.toString());
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer c = Integer.valueOf(tv.getText().toString())+1;
+                tv.setText(c.toString());
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("0");
+            }
+        });
+
     }
 }
