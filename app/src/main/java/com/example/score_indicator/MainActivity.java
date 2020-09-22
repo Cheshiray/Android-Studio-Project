619d0cebd75bbafbb96729c5ee7dfa6e91a24181
Cheshiray <cheshiray@163.com>
2020-09-22 11:03:05 +0800

    score-indicator

--- app/src/main/java/com/example/score_indicator/MainActivity.java
+++ app/src/main/java/com/example/score_indicator/MainActivity.java
@@ -12,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btn2;
     private Button btn3;
     private Button btn4;
-    private TextView tv;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private TextView tv1;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,36 +27,64 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.button2);
         btn3 = findViewById(R.id.button3);
         btn4 = findViewById(R.id.button4);
-        tv = findViewById(R.id.textView);
+        tv1 = findViewById(R.id.textView);
+        btn5 = findViewById(R.id.button8);
+        btn6 = findViewById(R.id.button7);
+        btn7 = findViewById(R.id.button5);
+        tv2 = findViewById(R.id.textView4);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer a = Integer.valueOf(tv.getText().toString())+3;
-                tv.setText(a.toString());
+                Integer a = Integer.valueOf(tv1.getText().toString())+3;
+                tv1.setText(a.toString());
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer b = Integer.valueOf(tv.getText().toString())+2;
-                tv.setText(b.toString());
+                Integer b = Integer.valueOf(tv1.getText().toString())+2;
+                tv1.setText(b.toString());
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer c = Integer.valueOf(tv.getText().toString())+1;
-                tv.setText(c.toString());
+                Integer c = Integer.valueOf(tv1.getText().toString())+1;
+                tv1.setText(c.toString());
             }
         });
 
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer a = Integer.valueOf(tv2.getText().toString())+3;
+                tv2.setText(a.toString());
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer b = Integer.valueOf(tv2.getText().toString())+2;
+                tv2.setText(b.toString());
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer c = Integer.valueOf(tv2.getText().toString())+1;
+                tv2.setText(c.toString());
+            }
+        });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText("0");
+                tv1.setText("0");
+                tv2.setText("0");
             }
         });
 
